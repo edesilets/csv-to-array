a8bef8a446f24d62539a96afc9c5879b270406e4
Ethan Desilets <ethan.desilets@hhcc.com>
2016-06-08 19:28:13 -0400

    A small bit of cleanup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CSV to Array
+
+Very Hack way of converting eachline in a csv to an array.
+Each arrray then is written to a 'js' file. in the same directory as the csv.
+Code needs to be cleaned up and to use promises.
--- readfile.js
+++ readfile.js
@@ -5,41 +5,47 @@ const csv = require('csv-parser');
 
 console.log('Current Run Dir: ', __dirname, '\n');
 
-let files = ['/Users/ethan.desilets/Desktop/pf.csv', '/Users/ethan.desilets/Desktop/numbers.csv', '/Users/ethan.desilets/Desktop/newtest.csv', '/Users/ethan.desilets/Desktop/test.js'];
-let csvData = [];
+let fileName = 'actualfilename';
+let dir = '/Users/USERNAME/Desktop/';
+let files = [dir+fileName+'.csv', dir+filename+'.js'];
 
-var setup = csv({
-    raw: false, // do not decode to utf-8 strings
-    separator: ',', // specify optional cell separator
-    // quote: '"',     // specify optional quote character
-    // escape: '"',    // specify optional escape character (defaults to quote value)
-    // newline: '\n',  // specify a newline character
-    // strict: true    // require column length match headers length
-    headers: ['address', 'city', 'state', 'country', 'zip', 'phone', 'latitude', 'longitude'] // Specifing the headers
-});
 
-// Create File
-fs.open(files[3], "w", function(err, fd) {
-    // handle error
-    if (err) throw err
-    fs.close(fd, function(err) {
-        // handle error
-        console.log('Close/Created');
+// Make into a singular function that takes the directory and file name and outputs a js file with an arrray of data
+// let csvToArray = function (dir, saveName) {
+
+    let writeStream = fs.createWriteStream(files[1], 'utf8')
+    let csvData = [];
+    var setup = csv({
+        raw: false, // do not decode to utf-8 strings
+        separator: ',', // specify optional cell separator
+        // quote: '"',     // specify optional quote character
+        // escape: '"',    // specify optional escape character (defaults to quote value)
+        // newline: '\n',  // specify a newline character
+        // strict: true    // require column length match headers length
+        // headers: ['address', 'city', 'state', 'country', 'zip', 'phone', 'latitude', 'longitude'] // Specifing the headers
     });
-});
 
-let writeStream = fs.createWriteStream(files[3], 'utf8')
-
-fs.createReadStream(files[2], 'utf8')
-    .pipe(setup)
-    .on('data', function(row) {
-        let format = `    ['${row.address},${row.city},${row.state},${row.country}',${+row.latitude}, ${+row.longitude}],\n`;
-        writeStream.write(format);
-    })
-    .on('end', function() {
-        writeStream.end();
-
-        let fileRequireLine = `];\n\nmodule.exports = points;`;
-        fs.appendFile(files[3], fileRequireLine, (err) => (console.error(err)));
-        console.log('hello end of file');
+    // Create File
+    fs.open(files[1], "w", function(err, fd) {
+        if (err) throw err
+        fs.close(fd, function(err) {
+            console.log('Close/Created');
+        });
     });
+
+    fs.createReadStream(files[0], 'utf8')
+        .pipe(setup)
+        .on('data', function(row) {
+            console.log(row);
+            let format = `    [${fileName},${+row.Latitude}, ${+row.Longitude}, './img/${fileName}.jpg'],\n`;
+            writeStream.write(format);
+        })
+        .on('end', function() {
+            writeStream.end();
+
+            let fileRequireLine = `];\n\nmodule.exports = points;`;
+            fs.appendFile(files[1], fileRequireLine, (err) => (console.error(err)));
+
+            console.log('File Sealed');
+        });
+// };
